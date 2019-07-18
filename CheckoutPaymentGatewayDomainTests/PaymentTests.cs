49dbc5fd997b83e2a5882346883dd84870e46386
FM <fereshtemohsenian@gmail.com>
2019-07-18 11:43:27 +0200

    add some tests for domain-objects

---
+++ CheckoutPaymentGatewayDomainTests/PaymentTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using CheckoutPaymentGatewayDomain;
+using FluentAssertions;
+
+namespace CheckoutPaymentGatewayDomainTests
+{
+    public class PaymentTests
+    {
+        [Test]
+        public void Ctor_WithCardNull_ShouldThrowArgumentNullException()
+        {
+            Action act = () => new Payment(new Guid(), null, 100);
+            act.ShouldThrow<ArgumentNullException>();
+        }
+        [TestCase("")]
+        public void Ctor_WithCardNull_ShouldThrowArgumentException(string cardNo)
+        {
+            Action act = () => new Payment(new Guid(), new Card(cardNo, 1,2020, 142), 10);
+            act.ShouldThrow<ArgumentException>();
+        }
+    }
+}
