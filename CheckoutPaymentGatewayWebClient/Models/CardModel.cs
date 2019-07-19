53da236da56455dfe15c957c1a6fb4f32d6c1e1a
FM <fereshtemohsenian@gmail.com>
2019-07-19 08:17:43 +0200

    add models to WebClient

---
+++ CheckoutPaymentGatewayWebClient/Models/CardModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutPaymentGatewayWebClient.Models
+{
+    [DataContract(Name = "Card")]
+    public class CardModel
+    {
+        [DataMember]
+        public string CardNumber { get; set; }
+
+        [DataMember]
+        public int Month { get; set; }
+
+        [DataMember]
+        public int Year { get; set; }
+
+        [DataMember]
+        public int CVV { get; set; }
+    }
+}
---
+++ CheckoutPaymentGatewayWebClient/Models/PaymentModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutPaymentGatewayWebClient.Models
+{
+    [DataContract(Name = "Payment")]
+    public class PaymentModel
+    {
+        public Guid Identifier { get; set; }
+        public CardModel Card { get; set; }
+        public decimal Amount { get; set; }
+        public int Status { get; set; }
+    }
+}
